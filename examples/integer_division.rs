14fcfb06308411869d86fff595b9a9a7c0a446ef
senww.eth <spaceodyssey2077@gmail.com>
2023-08-07 17:48:58 -0500

    clean up comments

--- examples/integer_division.rs
+++ examples/integer_division.rs
@@ -19,22 +19,22 @@ pub struct CircuitInput {
     pub x: String, // field element, but easier to deserialize as a string
 }
 
-// this algorithm takes a public input x, computes x^2 + 72, and outputs the result as public output
+// public inputs:
+// * A non-negative integer x, which is guaranteed to be at most 16-bits
+
+// public outputs:
+// * The non-negative integer (x / 32), where "/" represents integer division.
 fn integer_division<F: ScalarField>(
     ctx: &mut Context<F>,
     input: CircuitInput,
     make_public: &mut Vec<AssignedValue<F>>,
 ) {
     let x = F::from_str_vartime(&input.x).expect("deserialize field element should not fail");
-    // `Context` can roughly be thought of as a single-threaded execution trace of a program we want to ZK prove. We do some post-processing on `Context` to optimally divide the execution trace into multiple columns in a PLONKish arithmetization
-    // More advanced usage with multi-threaded witness generation is possible, but we do not explain it here
 
     let lookup_bits =
         var("LOOKUP_BITS").unwrap_or_else(|_| panic!("LOOKUP_BITS not set")).parse().unwrap();
-    // first we load a number `x` into as system, as a "witness"
     let x = ctx.load_witness(x);
-    // by default, all numbers in the system are private
-    // we can make it public like so:
+
     make_public.push(x);
 
     let range = RangeChip::default(lookup_bits);
@@ -53,6 +53,5 @@ fn main() {
 
     let args = Cli::parse();
 
-    // run different zk commands based on the command line arguments
     run(integer_division, args);
 }
